b33f4442dbd364b3527ba68ae0d16f813f881cbe
Filipp <bobrovich.fil@mail.ru>
2020-03-03 11:17:36 +0300

    add first test

--- test_main_page.py
+++ test_main_page.py
@@ -1,16 +1,8 @@
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+from pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-
-
-def test_add_to_cart(browser):
-    page = ProductPage(url="http://selenium1py.pythonanywhere.com/", browser)  # инициализируем объект Page Object
-    page.open()  # открываем страницу в браузере
-    page.should_be_add_to_cart_button()  # проверяем что есть кнопка добавления в корзину
-    page.add_product_to_cart()  # жмем кнопку добавить в корзину
-    page.should_be_success_message()  # проверяем что есть сообщение с нужным текстом
+    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
