77c2f5831be95661d54cd36f490293e01883ae7c
Filipp <bobrovich.fil@mail.ru>
2020-03-04 17:38:41 +0300

    change tests

--- test_main_page.py
+++ test_main_page.py
@@ -5,13 +5,9 @@ url = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbo
 
 
 def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, url,
-                    3)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page = MainPage(browser, url, 3)
     page.open()  # открываем страницу
     page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
     page.should_be_login_link()
-
-
-def test_second_guest_can_go_to_login_page(browser):
-    page2 = LoginPage(browser, url, 3)
-    page2.should_be_login_page()
+    login_page = LoginPage(browser, url)
+    login_page.should_be_login_page()
